e043ea791860e9951830a2acf4695ea1ce845bf5
SrStamm <mirkostamm19@gmail.com>
2025-05-28 22:39:00 +0100

    Enable Redis client for Docker and comment out local Redis client for testing

--- db/database.py
+++ db/database.py
@@ -35,7 +35,7 @@ def get_session():
         session.close()
 
 # Usar para Docker
-# redis_client = redis.Redis(host='redis', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 # Usar para Testing
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+# redis_client = redis.Redis(host='localhost', port=6379, db=0)
