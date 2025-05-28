683c4afc048de717871a6859f71d19fe22d883c0
SrStamm <mirkostamm19@gmail.com>
2025-05-28 22:09:21 +0100

    Enhance CI workflow with PostgreSQL and Redis service configurations, update test script to use Docker containers for database services, and adjust Redis client initialization for Docker compatibility.

--- src/ci-test.sh
+++ src/ci-test.sh
@@ -4,7 +4,9 @@ set -e  # Para que falle si algún comando falla
 echo "🚀 Iniciando tests..."
 
 echo "📦 Levantando contenedores desde docker-compose.test.yml"
-docker compose -f docker-compose.test.yml up --build -d
+# docker compose -f docker-compose.test.yml up --build -d
+docker run --name task-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=amaterasu -e POSTGRES_DB=database -p 5432:5432 -d postgres:latest
+docker run -d --name redis-stack -p 6379:6379 redis/redis-stack:latest
 
 echo "⏱ Esperando servicios..."
 sleep 5
