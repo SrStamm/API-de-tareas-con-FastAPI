1c75a4a918ec2035f18fcb2e67b0d6c6f39f0ff2
SrStamm <mirkostamm19@gmail.com>
2025-06-18 15:43:30 +0100

    Refactor database connection setup: streamline environment variable usage for PostgreSQL and Redis, and enhance Redis client configuration.

--- db/database.py
+++ db/database.py
@@ -2,19 +2,25 @@ from sqlmodel import SQLModel, create_engine, Session, select, or_
 from sqlalchemy import text, func
 from sqlalchemy.orm import selectinload, joinedload, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+import redis.asyncio as redis
 from dotenv import load_dotenv
 import os
 from models import db_models
+from urllib.parse import urlparse
 
 load_dotenv()
 
 user = os.environ.get('POSTGRES_USER')
 db_name = os.environ.get('POSTGRES_DB')
 password = os.environ.get('POSTGRES_PASSWORD')
+db_host = os.environ.get('POSTGRES_HOST')
+db_port = os.environ.get('POSTGRES_PORT')
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # url = f'postgresql+psycopg2://{user}:{password}@localhost:5432/{db_name}'
 
-url = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://postgres:amaterasu@postgres:5432/database')
+url = os.environ.get("DATABASE_URL", f'postgresql+psycopg2://{user}:amaterasu@{db_host}:{db_port}/{db_name}')
 engine = create_engine(url) # echo=True, pool_pre_ping=True 
 
 def create_db_and_tables():
@@ -42,9 +48,26 @@ redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", '6379'))
 redis_db = int(os.getenv("REDIS_DB", '0'))
 
-# En CI, usa una conexión síncrona para simplificar
-import redis.asyncio as redis
-redis_client = redis.Redis(
-        host=os.getenv("REDIS_HOST", "localhost"), 
-        port=int(os.getenv("REDIS_PORT", '6379')), 
-        db=int(os.getenv("REDIS_DB", '0')))
+redis_url = os.getenv("REDIS_URL")
+
+if redis_url:
+    parsed = urlparse(redis_url)
+
+    redis_host = parsed.hostname
+    redis_port = parsed.port
+    redis_password = parsed.password
+    redis_db = int(parsed.path.replace("/", "") or 0)
+
+    redis_client = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_password,
+        db=redis_db
+    )
+else:
+    # fallback local para desarrollo
+    redis_client = redis.Redis(
+        host=os.getenv("REDIS_HOST", "localhost"),
+        port=int(os.getenv("REDIS_PORT", 6379)),
+        db=int(os.getenv("REDIS_DB", 0))
+    )
